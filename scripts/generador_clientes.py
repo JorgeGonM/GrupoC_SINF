6d088494a611bfbd71b61fc4e0170e9dbaeaa12f
pablorejo <pablopiorejoiglesias@gmail.com>
2023-05-11 11:56:53 +0200

    poniendo scripts

---
+++ scripts/generador_clientes.py
@@ -0,0 +1,30 @@
+import sys
+import random
+import string
+
+def generar_cliente():
+    # Generamos un ID de cliente aleatorio
+    id_cliente = ''.join(random.choices(string.digits, k=8))
+    
+    # Generamos un correo electrónico aleatorio
+    email = ''.join(random.choices(string.ascii_lowercase, k=10)) + "@" + ''.join(random.choices(string.ascii_lowercase, k=5)) + "." + ''.join(random.choices(string.ascii_lowercase, k=3))
+    
+    # Creamos la línea de INSERT con los datos generados
+    linea_insert = "INSERT INTO Cliente (IDCliente, Email) VALUES('" + id_cliente + "','" + email + "');"
+    
+    return linea_insert
+
+
+
+# Obtenemos el número de líneas de INSERT a generar a partir del argumento de línea de comandos
+if len(sys.argv) > 1:
+    num_inserts = int(sys.argv[1])
+else:
+    print("Error, se tie")
+
+# Abrimos el archivo en modo de escritura
+with open('../datos/clientes.sql', 'w') as archivo:
+    # Generamos 10 líneas de INSERT con datos aleatorios y las escribimos en el archivo
+    for i in range(num_inserts):
+        linea_insert = generar_cliente()
+        archivo.write(linea_insert + '\n')
---
+++ scripts/generador_reservas.py
@@ -0,0 +1,35 @@
+import sys
+import random
+import string
+
+def generar_entrada():
+    # Generamos un ID de cliente aleatorio
+    
+    
+    id_evento = ''.join(random.choices(string.digits, k=8))
+    id_localidad = ''.join(random.choices(string.digits, k=8))
+    id_cliente = ''.join(random.choices(string.digits, k=8))
+    tipoUser = random.choice(['Bebe', 'Infantil', 'Adulto', 'Parado', 'Jubilado'])
+    
+    # Creamos la línea de INSERT con los datos generados
+    linea_insert = "call reservaDeEntradas ('" + id_evento + "','" + id_localidad + "','" + id_cliente + "','" + tipoUser + "');"
+    
+    return linea_insert
+
+
+                      
+
+# Obtenemos el número de líneas de INSERT a generar a partir del argumento de línea de comandos
+if len(sys.argv) > 1:
+    num_entradas = int(sys.argv[1])
+else:
+    print("Error, se tie")
+
+# Abrimos el archivo en modo de escritura
+
+with open('../datos/insertarReservaEntradas.sql', 'w') as archivo:
+    archivo.write("-- id_evento id_localidad id_cliente metodoDePago numCuenta tipoUser\n")
+    # Generamos 10 líneas de INSERT con datos aleatorios y las escribimos en el archivo
+    for i in range(num_entradas):
+        linea_insert = generar_entrada()
+        archivo.write(linea_insert + '\n')
